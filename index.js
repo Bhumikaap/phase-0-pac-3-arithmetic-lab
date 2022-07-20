ffe8fedcb5566b43f3f4571f089fc12b1b0e6497
Bhumika <patelbhumika19@gmail.com>
2022-07-20 13:21:56 +1000

    completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+function mul(a,b){
+    return a*b;
+}
+
+function div(a,b){
+    return a/b;
+}
+
+function increment(n){
+    
+    return (n=n+1);
+}
+
+function decrement(n){
+    return (n=n-1);
+}
+
+function makeInt(string){
+    return(parseInt(string));
+}
+
+function preserveDecimal(string){
+    return (parseFloat(string));
+}
